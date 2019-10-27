7623bbf0696a832202beeae88e6c8ed9c06a7be9
Kersten Schaller <kersten.schaller@outlook.com>
2019-10-27 12:14:31 +0100

    again deleted old files

---
+++ InformativeWallpaper/WallpaperConfigPanelTestForm/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WallpaperConfigPanelTestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wallpaperConfigControl1 = new InformativeWallpaper.WallpaperConfigControl();
+            this.SuspendLayout();
+            // 
+            // wallpaperConfigControl1
+            // 
+            this.wallpaperConfigControl1.Location = new System.Drawing.Point(12, 41);
+            this.wallpaperConfigControl1.Name = "wallpaperConfigControl1";
+            this.wallpaperConfigControl1.Size = new System.Drawing.Size(498, 184);
+            this.wallpaperConfigControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 386);
+            this.Controls.Add(this.wallpaperConfigControl1);
+            this.Name = "Form1";
+            this.Text = "Tesform";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private InformativeWallpaper.WallpaperConfigControl wallpaperConfigControl1;
+    }
+}
+
---
+++ InformativeWallpaper/code_src/WallpaperConfigControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InformativeWallpaper
+{
+    public partial class WallpaperConfigControl : UserControl
+    {
+        public WallpaperConfigControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private void radioButton_leftScreen_StaticImage_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_leftScreen_StaticImage.Checked == false)
+            {
+                radioButton_leftScreen_ImageFromFolder.Checked = true;
+            }
+            else
+            {
+                radioButton_leftScreen_ImageFromFolder.Checked = false;
+            }
+        }
+
+        private void radioButton_leftScreen_ImageFromFolder_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_leftScreen_ImageFromFolder.Checked == false)
+            {
+                radioButton_leftScreen_StaticImage.Checked = true;
+            }
+            else
+            {
+                radioButton_leftScreen_StaticImage.Checked = false;
+            }
+        }
+
+        private void radioButton_RightScreen_ImageFromFolder_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton_RightScreen_StaticImage_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton_rightScreen_staticImage_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton_rightScreen_ImageFromFolder_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
